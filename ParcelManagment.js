d12a504b0819c327b782f4b90fec206409a5af75
kusumavishu <vishukusuma2003@gmail.com>
2025-07-18 09:53:33 +0530

    Commit on Fri, Jul 18 09:53 | test : testing push

--- ParcelManagment.js
+++ ParcelManagment.js
@@ -6,7 +6,6 @@
 
 // ParcelRespose
 
-
 //ScreenTab >> Incoming Parcels
 // OnGoing
 // Allow
@@ -14,162 +13,180 @@
 // LeaveAtGate
 // StoredInSecurity
 
-
 // ScreenTab >> Delivered Parcels
 // DeliveredReturned
 // GivenToUser
 // ReturnedRejected
 
-
+// testing react native notification
 
 const parcelNotificationMap = {
-
-    PARCEL_CREATED: {
-        //Security >> user
-        //status :  OnGoing
-        title: "New Parcel Delivery",
-        message: "You have a delivery: Biriyani Delivery by Ramesh. Please respond: Allow, Leave at Gate, or Deny.",
-        data: {
-            screen: "My Parcels",
-            params: JSON.stringify({ ScreenTab: "Incoming Parcels", parcel_id: "1" }),
-        },
-        categoryId: "ParcelRespose",
-        channelId: "Parcel",
-        sound: "bellnotificationsound.wav",
+  PARCEL_CREATED: {
+    //Security >> user
+    //status :  OnGoing
+    title: "New Parcel Delivery",
+    message:
+      "You have a delivery: Biriyani Delivery by Ramesh. Please respond: Allow, Leave at Gate, or Deny.",
+    data: {
+      screen: "My Parcels",
+      params: JSON.stringify({ ScreenTab: "Incoming Parcels", parcel_id: "1" }),
     },
+    categoryId: "ParcelRespose",
+    channelId: "Parcel",
+    sound: "bellnotificationsound.wav",
+  },
 
-    PARCEL_ALLOWED: {
-        //user >> Security
-        //status :  Allow
-        title: "Parcel Approved by Resident",
-        message: "User of [flat_no] [block_name] has allowed the delivery. Please proceed with check-in.",
-        data: {
-            screen: "ParcelSecurity",
-            params: JSON.stringify({ ScreenTab: "Incoming Parcels", parcel_id: "123" }),
-        },
-        categoryId: "ParcelCheck-in",
-        channelId: "Parcel",
-        sound: "bellnotificationsound.wav",
+  PARCEL_ALLOWED: {
+    //user >> Security
+    //status :  Allow
+    title: "Parcel Approved by Resident",
+    message:
+      "User of [flat_no] [block_name] has allowed the delivery. Please proceed with check-in.",
+    data: {
+      screen: "ParcelSecurity",
+      params: JSON.stringify({
+        ScreenTab: "Incoming Parcels",
+        parcel_id: "123",
+      }),
     },
+    categoryId: "ParcelCheck-in",
+    channelId: "Parcel",
+    sound: "bellnotificationsound.wav",
+  },
 
-    PARCEL_CHECKED_IN: {
-        //Security >> user
-        //status :  CheckIn
-        title: "Delivery Checked In",
-        message: "The delivery person has been checked in for parcel #[parcel_id].",
-        data: {
-            screen: "My Parcels",
-            params: JSON.stringify({ ScreenTab: "Incoming Parcels", parcel_id: "123" }),
-        },
-        categoryId: "",
-        channelId: "Parcel",
-        sound: "bellnotificationsound.wav",
+  PARCEL_CHECKED_IN: {
+    //Security >> user
+    //status :  CheckIn
+    title: "Delivery Checked In",
+    message: "The delivery person has been checked in for parcel #[parcel_id].",
+    data: {
+      screen: "My Parcels",
+      params: JSON.stringify({
+        ScreenTab: "Incoming Parcels",
+        parcel_id: "123",
+      }),
     },
+    categoryId: "",
+    channelId: "Parcel",
+    sound: "bellnotificationsound.wav",
+  },
 
-    PARCEL_DELIVERED_RETURNED: {
-        //Security >> user
-        //status :  DeliveredReturned
-        title: "Parcel Delivered",
-        message: "The parcel #[parcel_id] has been delivered and the delivery person has exited.",
-        data: {
-            screen: "My Parcels",
-            params: JSON.stringify({ "ScreenTab": "Delivered Parcels", parcel_id: "123" }),
-        },
-        categoryId: "",
-        channelId: "Parcel",
-        sound: "bellnotificationsound.wav",
+  PARCEL_DELIVERED_RETURNED: {
+    //Security >> user
+    //status :  DeliveredReturned
+    title: "Parcel Delivered",
+    message:
+      "The parcel #[parcel_id] has been delivered and the delivery person has exited.",
+    data: {
+      screen: "My Parcels",
+      params: JSON.stringify({
+        ScreenTab: "Delivered Parcels",
+        parcel_id: "123",
+      }),
     },
+    categoryId: "",
+    channelId: "Parcel",
+    sound: "bellnotificationsound.wav",
+  },
 
+  //LEAVE_AT_GATE
+  PARCEL_LEAVE_AT_GATE: {
+    //user >> Security
+    //status :  LeaveInGate
+    title: "Leave at Gate Confirmed",
+    message:
+      "User of [flat_no] [block_name] requested the parcel be left at the gate. Please store it safely.",
+    data: {
+      screen: "My Parcels",
+      params: JSON.stringify({
+        ScreenTab: "Incoming Parcels",
+        parcel_id: "123",
+      }),
+    },
+    categoryId: "Parcelstore",
+    channelId: "Parcel",
+    sound: "bellnotificationsound.wav",
+  },
+  PARCEL_STORED_IN_SECURITY: {
+    //Security >> user
+    //status :StoredInSecurity
+    title: "Parcel Stored at Security",
+    message: "The parcel #[parcel_id] has been stored at the security desk.",
+    data: {
+      screen: "My Parcels",
+      params: JSON.stringify({
+        ScreenTab: "Incoming Parcels",
+        parcel_id: "123",
+      }),
+    },
+    categoryId: "ParcelStatusUpdates",
+    channelId: "Parcel",
+    sound: "bellnotificationsound.wav",
+  },
+  PARCEL_GIVEN_TO_USER: {
+    //Security >> user
+    //status :GivenToUser
+    title: "Parcel Handed Over",
+    message: "The parcel #[parcel_id] has been handed over to the user.",
+    data: {
+      screen: "My Parcels",
+      params: JSON.stringify({
+        ScreenTab: "Delivered Parcels",
+        parcel_id: "123",
+      }),
+    },
+    categoryId: "ParcelStatusUpdates",
+    channelId: "Parcel",
+    sound: "bellnotificationsound.wav",
+  },
 
-    //LEAVE_AT_GATE 
-    PARCEL_LEAVE_AT_GATE: {
-        //user >> Security
-        //status :  LeaveInGate
-        title: "Leave at Gate Confirmed",
-        message: "User of [flat_no] [block_name] requested the parcel be left at the gate. Please store it safely.",
-        data: {
-            screen: "My Parcels",
-            params: JSON.stringify({ ScreenTab: "Incoming Parcels", parcel_id: "123" }),
-        },
-        categoryId: "Parcelstore",
-        channelId: "Parcel",
-        sound: "bellnotificationsound.wav",
+  PARCEL_DENIED: {
+    //user >> Security
+    //status :Deny
+    title: "Parcel Denied by Resident",
+    message:
+      "User of [flat_no] [block_name] denied the parcel #[parcel_id]. Please return it to the delivery person.",
+    data: {
+      screen: "My Parcels",
+      params: JSON.stringify({ parcel_id: "123" }),
     },
-    PARCEL_STORED_IN_SECURITY: {
-        //Security >> user
-        //status :StoredInSecurity
-        title: "Parcel Stored at Security",
-        message: "The parcel #[parcel_id] has been stored at the security desk.",
-        data: {
-            screen: "My Parcels",
-            params: JSON.stringify({ ScreenTab: "Incoming Parcels", parcel_id: "123" }),
-        },
-        categoryId: "ParcelStatusUpdates",
-        channelId: "Parcel",
-        sound: "bellnotificationsound.wav",
+    categoryId: "ParcelDenied",
+    channelId: "Parcel",
+    sound: "bellnotificationsound.wav",
+  },
+  PARCEL_RETURNED_REJECTED: {
+    //Security >> user
+    //status :ReturnedRejected
+    title: "Parcel Returned to Delivery Person",
+    message: "The parcel #[parcel_id] has been returned after user rejection.",
+    data: {
+      screen: "My Parcels",
+      params: JSON.stringify({
+        ScreenTab: "Delivered Parcels",
+        parcel_id: "123",
+      }),
     },
-    PARCEL_GIVEN_TO_USER: {
-        //Security >> user
-        //status :GivenToUser
-        title: "Parcel Handed Over",
-        message: "The parcel #[parcel_id] has been handed over to the user.",
-        data: {
-            screen: "My Parcels",
-            params: JSON.stringify({ "ScreenTab": "Delivered Parcels", parcel_id: "123" }),
-        },
-        categoryId: "ParcelStatusUpdates",
-        channelId: "Parcel",
-        sound: "bellnotificationsound.wav",
-    },
-
-
-    PARCEL_DENIED: {
-        //user >> Security
-        //status :Deny
-        title: "Parcel Denied by Resident",
-        message: "User of [flat_no] [block_name] denied the parcel #[parcel_id]. Please return it to the delivery person.",
-        data: {
-            screen: "My Parcels",
-            params: JSON.stringify({ parcel_id: "123" }),
-        },
-        categoryId: "ParcelDenied",
-        channelId: "Parcel",
-        sound: "bellnotificationsound.wav",
-    },
-    PARCEL_RETURNED_REJECTED: {
-        //Security >> user
-        //status :ReturnedRejected
-        title: "Parcel Returned to Delivery Person",
-        message: "The parcel #[parcel_id] has been returned after user rejection.",
-        data: {
-            screen: "My Parcels",
-            params: JSON.stringify({ "ScreenTab": "Delivered Parcels", parcel_id: "123" }),
-        },
-        categoryId: "ParcelStatusUpdates",
-        channelId: "Parcel",
-        sound: "bellnotificationsound.wav",
-    }
+    categoryId: "ParcelStatusUpdates",
+    channelId: "Parcel",
+    sound: "bellnotificationsound.wav",
+  },
 };
 
 function getNotificationMessageAndDataParcelManagment(key) {
-
-    const notification = parcelNotificationMap[key];
-    if (!notification) {
-        throw new Error(`Notification type "${key}" not found.`);
-    }
-    return notification;
+  const notification = parcelNotificationMap[key];
+  if (!notification) {
+    throw new Error(`Notification type "${key}" not found.`);
+  }
+  return notification;
 }
 
 module.exports = {
-    getNotificationMessageAndDataParcelManagment,
-    parcelNotificationMap
+  getNotificationMessageAndDataParcelManagment,
+  parcelNotificationMap,
 };
 
-
-const AnalogueLogo = 'https://www.analogueitsolutions.com/assets/analogueitsolution_logo-BVdZYf9n.png';
-
-
-
+const AnalogueLogo =
+  "https://www.analogueitsolutions.com/assets/analogueitsolution_logo-BVdZYf9n.png";
 
 // Workflow	Status	Tab	Suggested Label
 // Allow	OnGoing	Incoming Parcels	"Waiting for your action"
